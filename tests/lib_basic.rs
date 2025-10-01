34c2e4d0640b48456d66742c3e4ee55e3aa36923
Maxime Guillemin <guimaxali@gmail.com>
2025-10-01 23:36:28 +0200

    Refresh tests for scheduler refactor

--- tests/lib_basic.rs
+++ tests/lib_basic.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
-use astreinte::{Scheduler, scheduler::AssignOptions, model::Person};
+use astreinte::{model::Person, scheduler::AssignOptions, Scheduler};
 use chrono::{TimeZone, Utc};
 
 #[test]
@@ -9,15 +9,16 @@ fn create_and_assign_basic() {
     let b = Person::new("bob", "Bob");
     s.add_people(vec![a.clone(), b.clone()]);
 
-    let t0 = Utc.with_ymd_and_hms(2025,10,1,8,0,0).unwrap();
-    let t1 = Utc.with_ymd_and_hms(2025,10,1,20,0,0).unwrap();
-    let t2 = Utc.with_ymd_and_hms(2025,10,2,8,0,0).unwrap();
-    let t3 = Utc.with_ymd_and_hms(2025,10,2,20,0,0).unwrap();
+    let t0 = Utc.with_ymd_and_hms(2025, 10, 1, 8, 0, 0).unwrap();
+    let t1 = Utc.with_ymd_and_hms(2025, 10, 1, 20, 0, 0).unwrap();
+    let t2 = Utc.with_ymd_and_hms(2025, 10, 2, 8, 0, 0).unwrap();
+    let t3 = Utc.with_ymd_and_hms(2025, 10, 2, 20, 0, 0).unwrap();
 
     s.create_shift("nuit1", t0, t1).unwrap();
     s.create_shift("nuit2", t2, t3).unwrap();
 
-    s.assign_rotative(&[a,b], AssignOptions::default()).unwrap();
+    s.assign_rotative(&[a, b], AssignOptions::default())
+        .unwrap();
     let roster = s.roster();
     assert_eq!(roster.shifts.len(), 2);
     assert!(roster.shifts[0].assigned.is_some());
@@ -30,10 +31,10 @@ fn detect_overlap_conflict() {
     let a = Person::new("alice", "Alice");
     s.add_people(vec![a.clone()]);
 
-    let t0 = Utc.with_ymd_and_hms(2025,10,1,8,0,0).unwrap();
-    let t1 = Utc.with_ymd_and_hms(2025,10,1,12,0,0).unwrap();
-    let t2 = Utc.with_ymd_and_hms(2025,10,1,10,0,0).unwrap();
-    let t3 = Utc.with_ymd_and_hms(2025,10,1,14,0,0).unwrap();
+    let t0 = Utc.with_ymd_and_hms(2025, 10, 1, 8, 0, 0).unwrap();
+    let t1 = Utc.with_ymd_and_hms(2025, 10, 1, 12, 0, 0).unwrap();
+    let t2 = Utc.with_ymd_and_hms(2025, 10, 1, 10, 0, 0).unwrap();
+    let t3 = Utc.with_ymd_and_hms(2025, 10, 1, 14, 0, 0).unwrap();
 
     let id1 = s.create_shift("A", t0, t1).unwrap();
     let id2 = s.create_shift("B", t2, t3).unwrap();
@@ -41,8 +42,16 @@ fn detect_overlap_conflict() {
     // assigne manuellement
     {
         let r = s.roster_mut();
-        r.shifts.iter_mut().find(|sh| sh.id == id1).unwrap().assigned = Some(a.id.clone());
-        r.shifts.iter_mut().find(|sh| sh.id == id2).unwrap().assigned = Some(a.id.clone());
+        r.shifts
+            .iter_mut()
+            .find(|sh| sh.id == id1)
+            .unwrap()
+            .assigned = Some(a.id.clone());
+        r.shifts
+            .iter_mut()
+            .find(|sh| sh.id == id2)
+            .unwrap()
+            .assigned = Some(a.id.clone());
     }
 
     let conflicts = s.detect_conflicts(AssignOptions::default());
